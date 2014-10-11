5604c54c9d7fe016b3b25431c1b6baeb38222ff9
Atanas <atanas.pam@gmail.com>
2014-10-11 14:07:06 +0100

    Add dictionary

---
+++ Dictionary/word_dict.py
@@ -0,0 +1,5 @@
+
+
+
+dictionary dict = {"am" : "http://weareselecters.com/files/images/2011/12/iam.jpg", "calm" : "http://upload.wikimedia.org/wikipedia/commons/6/6f/Keep-calm-and-carry-on-scan.jpg",
+                    "saw" : "http://s3.amazonaws.com/rapgenius/rESgEbNcSlKYh53R6rQe_saw_movie.jpg" }
