6ad155ca6185b087a72b4d6c4d62976dbf88670e
Atanas <atanas.pam@gmail.com>
2014-10-11 16:05:45 +0100

    Added Module1.py

---
+++ Modules/Module1.py
@@ -0,0 +1,26 @@
+
+
+def process(rawString):
+    splitString = []
+    splitString = rawString.split(" ")
+    
+    dictionary = {}
+    
+    for i in range (0, len(splitString)-1)
+        #if dictionary alreafy has this element replace it and continue
+        if dictionary.haskey(splitString[i])
+            splitString[i] = dictionary[splitString[i]]
+            continue
+        else
+            #add the element in the dictionary
+            dictionary = dictionary.update(splitString[i] : str(getLink(splitString[i]))
+            # after that for the same element add its link to the array
+            splitString[i] = dictionary[splitString[i]]
+    return splitString
+    
+def getLink(word)
+
+    return result
+    
+    
+    
